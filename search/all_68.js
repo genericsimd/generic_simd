c372c5b57b55f6d221b628a9c15c1c735349369d
Peng Wu <pengwu@us.ibm.com>
2013-09-23 16:37:39 -0400

    regenerated doxygen docs

--- search/all_68.js
+++ search/all_68.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['helper',['Helper',['../structvsx_1_1svec_3_014_00_01bool_01_4_1_1Helper.html',1,'vsx::svec&lt; 4, bool &gt;']]],
-  ['helper',['Helper',['../structvsx_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a891aee9ed2450e582305b8ee586c57b3',1,'vsx::svec&lt; 4, bool &gt;::Helper::Helper()'],['../structgeneric_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a77c71cb8d47b2d48a078980b5ac1dcaa',1,'generic::svec&lt; 4, bool &gt;::Helper::Helper()'],['../structsse_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a9a962af4cd828d41d0dd6989908bc87f',1,'sse::svec&lt; 4, bool &gt;::Helper::Helper()']]],
+  ['helper',['Helper',['../structvsx_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a048d6d7042de0e6843647c17eaca3878',1,'vsx::svec&lt; 4, bool &gt;::Helper::Helper()'],['../structgeneric_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a43dc46fe81dafe46e0a87f3d914e3805',1,'generic::svec&lt; 4, bool &gt;::Helper::Helper()'],['../structsse_1_1svec_3_014_00_01bool_01_4_1_1Helper.html#a6c659448cc32e502ea60ba06eace5eec',1,'sse::svec&lt; 4, bool &gt;::Helper::Helper()']]],
   ['helper',['Helper',['../structsse_1_1svec_3_014_00_01bool_01_4_1_1Helper.html',1,'sse::svec&lt; 4, bool &gt;']]],
   ['helper',['Helper',['../structgeneric_1_1svec_3_014_00_01bool_01_4_1_1Helper.html',1,'generic::svec&lt; 4, bool &gt;']]]
 ];
--- search/all_6e.js
+++ search/all_6e.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['none_5ftrue',['none_true',['../structvsx_1_1svec_3_014_00_01bool_01_4.html#a69e6f899dd1120cec39f0f560fa064a2',1,'vsx::svec&lt; 4, bool &gt;::none_true()'],['../structgeneric_1_1svec_3_014_00_01bool_01_4.html#aa154a5d469c79250278e10ce46a64b2a',1,'generic::svec&lt; 4, bool &gt;::none_true()'],['../structsse_1_1svec_3_014_00_01bool_01_4.html#aa9d4b8304f014a6e8f88f53e78a9346a',1,'sse::svec&lt; 4, bool &gt;::none_true()']]]
+  ['none_5ftrue',['none_true',['../structvsx_1_1svec_3_014_00_01bool_01_4.html#ab5f31cda52261a88fcf72d088f10c681',1,'vsx::svec&lt; 4, bool &gt;::none_true()'],['../structgeneric_1_1svec_3_014_00_01bool_01_4.html#a8a1d60a5245235c6a0038e42ef96e3b9',1,'generic::svec&lt; 4, bool &gt;::none_true()'],['../structsse_1_1svec_3_014_00_01bool_01_4.html#a8384aec72950884cddd3d49fef57c184',1,'sse::svec&lt; 4, bool &gt;::none_true()']]]
 ];
